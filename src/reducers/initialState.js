b14801d795974dfc284a88d42f24983222c77d2c
Narek M <narekmamikonyan@gmail.com>
2016-11-19 16:32:41 +0400

    Fixed readme and initial state

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,7 +1,7 @@
 export default {
-  events: [{id: 1, from: '2016-11-01', to: '2016-11-01', description: 'Tutuz'},
-    {id: 3, from: '2016-11-01', to: '2016-11-01', description: 'BOOHA HA HA NIGGAS'},
-    {id: 2, from: '2016-11-05', to: '2016-11-07', description: 'Esor gnum enq xmelu cter'}],
+  events: [{id: 1, from: '2016-11-01', to: '2016-11-01', description: 'Jack"s birthday'},
+    {id: 3, from: '2016-11-01', to: '2016-11-01', description: 'Exams'},
+    {id: 2, from: '2016-11-05', to: '2016-11-07', description: 'Day off'}],
 
   currentDate: null
 };
